8536ff22d6d4992be90c8962a47d9f507d71f9ee
ArcKontyR <kontyrtime@mail.ru>
2025-04-21 19:37:54 +0500

    initial

---
+++ client/src/views/SupervisorView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1> supervisor </h1>
+  </main>
+</template>
---
+++ client/vite.config.ts
@@ -0,0 +1,42 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig((env) => {
+  const envars = loadEnv(env.mode, './');
+
+  const serverURL = new URL(
+    envars.VITE_SERVER_URL ?? '<http://localhost:3001>'
+  );
+  const serverAPIPath = envars.VITE_SERVER_API_PATH ?? '/api';
+
+  return {
+    envDir: './',
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+    ],
+    // make the API path globally available in the client
+    define: {
+      __API_PATH__: JSON.stringify(serverAPIPath),
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        // proxy requests with the API path to the server
+        // <http://localhost:5173/api> -> <http://localhost:3001/api>
+        [serverAPIPath]: serverURL.origin,
+      },
+    },
+  }
+})
